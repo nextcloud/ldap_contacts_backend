1042b628711930f489e52721301a963f80e0fab6
Côme Chilliet <come.chilliet@nextcloud.com>
2022-04-28 15:15:08 +0200

    Move tests up a folder
    Signed-off-by: Côme Chilliet <come.chilliet@nextcloud.com>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -26,7 +26,7 @@ if (!defined('PHPUNIT_RUN')) {
         define('PHPUNIT_RUN', 1);
 }
 
-require_once __DIR__.'/../../../../lib/base.php';
+require_once __DIR__.'/../../../lib/base.php';
 \OC::$loader->addValidRoot(\OC::$SERVERROOT . '/tests');
 if(!class_exists('PHPUnit\Framework\TestCase')) {
         require_once('PHPUnit/Autoload.php');
