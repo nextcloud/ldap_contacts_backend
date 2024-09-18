58b2b276fa52b3248aebfdf3caf1389f06294ad7
Côme Chilliet <come.chilliet@nextcloud.com>
2024-09-18 17:48:56 +0200

    feat: Add rector configuration and vendor bin
    Signed-off-by: Côme Chilliet <come.chilliet@nextcloud.com>

---
+++ rector.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/appinfo',
+        __DIR__ . '/lib',
+        __DIR__ . '/tests',
+    ])
+    // uncomment to reach your current PHP version
+    // ->withPhpSets()
+    ->withTypeCoverageLevel(0);
